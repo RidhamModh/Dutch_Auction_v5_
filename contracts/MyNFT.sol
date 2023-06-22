6bd11c932c9b3cb87730f59087deec2509548135
Ridham Modh <ridhs193@gmail.com>
2023-06-22 09:58:38 -0700

    first commit

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Author: Raj Mehta
+// Version : 5.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721{
+    using Counters for Counters.Counter;
+    Counters.Counter public tokenIds;
+
+    constructor() ERC721("MyNFT", "MNFT"){}
+
+    function mintNFT(address recipient) public
+    {
+        tokenIds.increment();
+
+        uint newItem = tokenIds.current();
+        _safeMint(recipient, newItem);
+    }
+}
